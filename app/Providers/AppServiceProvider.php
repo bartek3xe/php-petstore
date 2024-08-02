aa2d234550c0012ecf9f694f1bdfa5d842de69d0
bartek3xe <bartek3xe@gmail.com>
2024-08-02 17:33:09 +0200

    Register service in app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\PetService;
+use GuzzleHttp\Client;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(Client::class, function ($app) {
+            return new Client(['base_uri' => PetService::BASE_API_URL]);
+        });
     }
 
     /**
