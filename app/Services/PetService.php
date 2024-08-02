ffb7bb4c4c5475df7430b16b179e1119cbb023af
bartek3xe <bartek3xe@gmail.com>
2024-08-02 17:22:42 +0200

    Implement PetInterface and add necessary methods

--- app/Services/PetService.php
+++ app/Services/PetService.php
@@ -4,9 +4,30 @@ declare(strict_types=1);
 
 namespace App\Services;
 
-class PetService
+use App\Contracts\PetServiceInterface;
+
+class PetService implements PetServiceInterface
 {
     private const BASE_API_URL = 'https://petstore.swagger.io/v2/';
 
-    
+
+    public function getAllPets()
+    {
+    }
+
+    public function getPetById(string $id)
+    {
+    }
+
+    public function createPet(array $data)
+    {
+    }
+
+    public function updatePet(array $data)
+    {
+    }
+
+    public function deletePet(string $id)
+    {
+    }
 }
