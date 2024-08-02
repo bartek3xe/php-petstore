6bbad17923425a5ae4190aa6b264ba4e23db7c73
bartek3xe <bartek3xe@gmail.com>
2024-08-02 17:32:05 +0200

    Use httpRequestService to get data

--- app/Services/PetService.php
+++ app/Services/PetService.php
@@ -10,24 +10,34 @@ class PetService implements PetServiceInterface
 {
     private const BASE_API_URL = 'https://petstore.swagger.io/v2/';
 
+    public function __construct(private readonly HttpRequestService $httpRequestService)
+    {
+    }
 
     public function getAllPets()
     {
+        return $this->httpRequestService->makeRequest('GET', 'pet/findByStatus', [
+            'query' => ['status' => 'available']
+        ]);
     }
 
-    public function getPetById(string $id)
+    public function getPetById(string $id): array
     {
+        return $this->httpRequestService->makeRequest('GET', "pet/{$id}");
     }
 
-    public function createPet(array $data)
+    public function createPet(array $data): array
     {
+        return $this->httpRequestService->makeRequest('POST', 'pet', ['json' => $data]);
     }
 
-    public function updatePet(array $data)
+    public function updatePet(array $data): array
     {
+        return $this->httpRequestService->makeRequest('PUT', 'pet', ['json' => $data]);
     }
 
-    public function deletePet(string $id)
+    public function deletePet(string $id): array
     {
+        return $this->httpRequestService->makeRequest('DELETE', "pet/{$id}");
     }
 }
